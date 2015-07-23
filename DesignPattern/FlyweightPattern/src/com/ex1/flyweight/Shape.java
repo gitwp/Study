e1e3e86171f56814a5c3f68bec0f823947b8e796
gitwp <534848221@qq.com>
2015-07-23 10:56:23 +0800

    FlyweightPattern

---
+++ DesignPattern/FlyweightPattern/src/com/ex1/flyweight/Shape.java
@@ -0,0 +1,5 @@
+package com.ex1.flyweight;
+
+public abstract class Shape {
+    public abstract void draw();
+}
---
+++ DesignPattern/FlyweightPattern/src/com/ex2/flyweight/Flyweight.java
@@ -0,0 +1,6 @@
+package com.ex2.flyweight;
+
+public interface Flyweight {
+	//一个示意性方法，参数state是外蕴状态
+    public void operation(String state);
+}
